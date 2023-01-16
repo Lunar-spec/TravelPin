364a237a63e7b6f89217728af63e7a47f0a4f21d
Manjesh Singh <smanjesh1854@gmail.com>
2023-01-16 17:52:08 +0530

    m

--- client/src/components/geocoder.js
+++ client/src/components/geocoder.js
@@ -1,5 +1,5 @@
 import MapBoxGeocoder from '@mapbox/mapbox-gl-geocoder';
-import { useControl, Marker } from 'react-map-gl'
+import { useControl } from 'react-map-gl'
 import mapboxgl from 'mapbox-gl'
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'
 import './geocoder.css'
